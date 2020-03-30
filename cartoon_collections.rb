560b585af6e9705cda9ee1e7f122aeb3aeb228c6
Lauren Birts <birtslauren@gmail.com>
2020-03-30 23:59:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
-   snacks.find { |maybe_cheese| cheese_types.include?(maybe_cheese) }
+   planeteer_calls.find { |maybe_calls| valid_calls.include?(maybe_calls) }
   # Return the first valid call found, or return nil if no valid calls are found
 end
